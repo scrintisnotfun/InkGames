d219c865c8827f4922b0ab8df0a8adbb4a815e98
Voltrex <168355591+scrintisnotfun@users.noreply.github.com>
2025-12-01 09:13:29 -0500

    Update sj.lua

--- sj.lua
+++ sj.lua
@@ -1,2 +1,1 @@
-wait(0.25)
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/e39c903cf4d81d50e9f906585f2b8d8f4a52044bdff0efdb6133ecd1b1a52041/download"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scrintisnotfun/InkGames/refs/heads/main/NewLoader.lua"))()
